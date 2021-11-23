8d42929b2d68f7e70421fb778e42aaaba0636232
Kleber Dias <kldias@gmail.com>
2021-11-23 16:11:18 -0300

    Source tree update

---
+++ app/src/main/java/br/com/alura/agenda/ui/ListaAlunosView.java
@@ -0,0 +1,53 @@
+package br.com.alura.agenda.ui;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.model.Aluno;
+import br.com.alura.agenda.ui.adapter.ListaAlunosAdapter;
+
+public class ListaAlunosView {
+
+    private final ListaAlunosAdapter adapter;
+    private final AlunoDAO dao;
+    private final Context context;
+
+    public ListaAlunosView(Context context) {
+        this.context = context;
+        this.adapter = new ListaAlunosAdapter(this.context);
+        this.dao = new AlunoDAO();
+
+    }
+
+    public void confirmaRemocao(final MenuItem item) {
+        new AlertDialog
+                .Builder(context)
+                .setTitle("Removendo aluno")
+                .setMessage("Tem certeza que quer remover o aluno?")
+                .setPositiveButton("Sim", (dialog, which) -> {
+                    AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                    Aluno alunoEscolhido = (Aluno) adapter.getItem(menuInfo.position);
+                    remove(alunoEscolhido);
+                })
+                .setNegativeButton("Não", null)
+                .show();
+    }
+
+    public void atualizaAlunos() {
+        adapter.atualiza(dao.todos());
+    }
+
+    public void remove(Aluno aluno) {
+        dao.remove(aluno);
+        adapter.remove(aluno);
+    }
+
+    public void configuraAdaper(ListView listaDeAlunos) {
+
+        listaDeAlunos.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -0,0 +1,114 @@
+package br.com.alura.agenda.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import br.com.alura.agenda.R;
+import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.model.Aluno;
+import br.com.alura.agenda.ui.ListaAlunosView;
+import br.com.alura.agenda.ui.adapter.ListaAlunosAdapter;
+
+import static br.com.alura.agenda.ui.activity.ConstantesActivities.CHAVE_ALUNO;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+    public static final String TITULO_APPBAR = "Lista de alunos";
+    private final AlunoDAO dao = new AlunoDAO();
+    private ListaAlunosAdapter adapter;
+    private final ListaAlunosView listaAlunosView = new ListaAlunosView(this);
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+
+        int itemId = item.getItemId();
+        if (itemId == R.id.activity_lista_alunos_menu_remover) {
+            listaAlunosView.confirmaRemocao(item);
+
+        }
+        return super.onContextItemSelected(item);
+    }
+
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater()
+                .inflate(R.menu.activity_lista_alunos_menu, menu);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+        setTitle(TITULO_APPBAR);
+        configuraFabNovoAluno();
+        configuraLista();
+
+    }
+
+    private void configuraFabNovoAluno() {
+        FloatingActionButton botaoNovoAluno = findViewById(R.id.activity_lista_alunos_fab_novo_aluno);
+        botaoNovoAluno.setOnClickListener(view -> abreFormularioModoInsereAluno());
+    }
+
+    private void abreFormularioModoInsereAluno() {
+        startActivity(new Intent(this, FormularioAlunoActivity.class));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        listaAlunosView.atualizaAlunos();
+    }
+
+
+
+    private void configuraLista() {
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_de_alunos_listview);
+//        final List<Aluno> alunos = dao.todos();
+        listaAlunosView.configuraAdaper(listaDeAlunos);
+        configuraListenerDeCliquePorItem(listaDeAlunos);
+//        configuraListenerDeCliqueLongoPorItem(listaDeAlunos);
+        registerForContextMenu(listaDeAlunos);
+    }
+
+//    private void configuraListenerDeCliqueLongoPorItem(ListView listaDeAlunos) {
+//        listaDeAlunos.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int posicao, long id) {
+//                Aluno alunoEscolhido = (Aluno) adapterView.getItemAtPosition(posicao);
+//                remove(alunoEscolhido);
+//                return false;
+//            }
+//        });
+//    }
+
+
+
+    private void configuraListenerDeCliquePorItem(ListView listaDeAlunos) {
+        listaDeAlunos.setOnItemClickListener((adapterView, view, posicao, id) -> {
+            Aluno alunoEscolhido = (Aluno) adapterView.getItemAtPosition(posicao);
+            abreFormularioModoEditaAluno(alunoEscolhido);
+
+        });
+    }
+
+    private void abreFormularioModoEditaAluno(Aluno alunoEscolhido) {
+        Intent vaiParaFormularioActivity = new Intent(ListaAlunosActivity.this, FormularioAlunoActivity.class);
+        vaiParaFormularioActivity.putExtra(CHAVE_ALUNO, alunoEscolhido);
+        startActivity(vaiParaFormularioActivity);
+    }
+
+
+}
